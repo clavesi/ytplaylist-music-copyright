e1ad50fc352efebee0dcab3d89009ffe112fabc0
clavesi <34937562+RealRavenn@users.noreply.github.com>
2020-12-16 22:40:42 -0600

    Tweaked layout design

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import youtube_dl
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def hello():
     url = 'https://www.youtube.com/watch?v=kOZoLTQzpfg'
 
@@ -14,4 +13,8 @@ def hello():
     # print('{} - {}'.format(video['artist'], video['track']))
     # https://colorpalettes.net/color-palette-4221/
 
-    return render_template('main.html', artist=video['artist'], song=video['track'])
+    # return render_template('main.html', artist=video['artist'], song=video['track'])
+    return render_template('main.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
