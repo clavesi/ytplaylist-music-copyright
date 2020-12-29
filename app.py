17c1165787283ec911a5c06ee1f23a2b4ec3383f
clavesi <34937562+RealRavenn@users.noreply.github.com>
2020-12-29 15:20:41 -0600

    Create cards for every song

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from os import error
 from flask import Flask, render_template, request
-import youtube_dl, time, json, flask
+import youtube_dl
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # https://colorpalettes.net/color-palette-4221/
     return render_template('main.html')
 
-@app.route('/process', methods=['POST'])
-def process():
-    # test - https://www.youtube.com/playlist?list=PLyEROMcux1lU6vV-axvuv52ldd5m3nwWG
-    # real - https://www.youtube.com/playlist?list=PLyEROMcux1lW4ybgltuzB2W-cmzCViSpR
-    # url = 'https://www.youtube.com/playlist?list=PLyEROMcux1lU6vV-axvuv52ldd5m3nwWG'
+@app.route('/videos', methods=['POST'])
+def videos():
     url = request.form['link']
 
+    #TODO Watch for privated or deleted videos
     # Extract videos from playlist and get info
     ydl = youtube_dl.YoutubeDL({})
     with ydl:
@@ -27,13 +24,11 @@ def process():
     for video in videos:
         try:
             if video['artist'] != None:
-                copyrightedVideos.append([video['artist'], video['track'], video['webpage_url']])
+                copyrightedVideos.append([video['title'], video['artist'], video['track'], video['webpage_url'], video['thumbnail']])
         except error:
             print(error)
 
-    # print(f'{len(copyrightedVideos)} out of {len(videos)} videos had obvious copyrighted music')
-    # print(copyrightedVideos)
-    return render_template('videos.html', videos=copyrightedVideos)
+    return render_template('main.html', videos=copyrightedVideos, numCopy=len(copyrightedVideos), numTotal=len(videos))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
